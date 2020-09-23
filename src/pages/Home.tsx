490d638ca3b34f1f2eb28fc898ff3895e4a52f91
Parth <dani.parth05@gmail.com>
2020-09-23 19:23:18 +0530

    full working project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,53 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { Plugins, CameraResultType } from '@capacitor/core';
+import { CleverTap } from '@ionic-native/clevertap';
+import React, { Component } from 'react';
+const { Camera } = Plugins;
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+const INITIAL_STATE = {
+  photo: '',
+};
+export class Home extends Component {
+  state: any = {};
+  props: any = {};
+  constructor(props: any) {
+    super(props);
+    CleverTap.setDebugLevel(2);
+    this.state = { ...INITIAL_STATE };
+  }
+  async takePicture() {
+          CleverTap.recordEventWithName("Ionic React Push Events");
+          const image = await Camera.getPhoto({
+          quality: 90,
+          allowEditing: false,
+          resultType: CameraResultType.Uri
+              });
+          var imageUrl = image.webPath;
+          // Can be set to the src of an image now
+              this.setState({
+                  photo: imageUrl
+              })
+            }
+
+  render() {
+    const { photo } = this.state;
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Ionic Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
-
+        <IonContent className="ion-padding">
+          <IonImg style={{ 'border': '1px solid black', 'minHeight': '100px' }} src={photo} ></IonImg>
+          <IonFab color="primary" vertical="bottom" horizontal="center" slot="fixed">
+            <IonFabButton color="primary" onClick={() => this.takePicture()}>
+              <IonIcon name="add" />
+            </IonFabButton>
+          </IonFab>
+        </IonContent>
+      </IonPage >
+    );
+  };
+}
 export default Home;
